01a26329bf9797919ae8729044cfa90abcb28dac
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-10-23 19:46:25 +0900

    カウントのたびにstateの更新（再レンダリング）

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App = () => {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const clickAddCount = () => {
+    setCount(count + 1);
+  };
   return (
     <>
-      <h1>Appこん</h1>
+      <h1>カウントのたびにstateの更新（再レンダリング）</h1>
+      <p>{count}</p>
+      <button onClick={clickAddCount}>カウントアップ</button>
     </>
   );
-};
-
-export default App;
+}
