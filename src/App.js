e9d6c9c929ca6a14f96a25c0eee5b4a138354672
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-10-24 22:49:22 +0900

    7. レンダリング確認用のコーディング

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
+import { ChildArea } from "./childArea";
 
 export default function App() {
   const [count, setCount] = useState(0);
   const clickAddCount = () => {
     setCount(count + 1);
   };
+
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
   return (
     <>
       <h1>カウントのたびにstateの更新（再レンダリング）</h1>
       <p>{count}</p>
       <button onClick={clickAddCount}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </>
   );
 }
---
+++ src/childArea.jsx
@@ -0,0 +1,15 @@
+const style = {
+  backgroundColor: "khaki"
+};
+export const ChildArea = (props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
